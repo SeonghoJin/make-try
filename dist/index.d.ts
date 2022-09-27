0c0f004c9d4b236dc056f26b51d364b5abb099b2
moretall <seongho4996@naver.com>
2022-09-27 15:30:27 +0900

    build and patch version up

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,5 @@
 export declare type Function = (...args: any[]) => any;
 export declare type AsyncFunction = (...args: any[]) => Promise<any>;
-export declare type UnPackPromise<T> = T extends Promise<infer Payload> ? Payload : T;
 export declare type Fullfiled<T> = {
     err: null;
     hasError: false;
@@ -11,8 +10,8 @@ export declare type Rejected = {
     hasError: true;
     err: unknown;
 };
-export declare type MakeReturnPayload<T> = Rejected | Fullfiled<UnPackPromise<T>>;
-export declare type IsAsyncFunction<T extends Function> = ReturnType<T> extends never ? false : T extends (...args: any[]) => infer R ? R extends Promise<any> ? true : false : false;
+export declare type MakeReturnPayload<T> = Rejected | Fullfiled<Awaited<T>>;
+export declare type IsAsyncFunction<T extends Function> = ReturnType<T> extends never ? false : ReturnType<T> extends Promise<any> ? true : false;
 export declare type MakeSyncReturn<T extends Function> = (...args: Parameters<T>) => MakeReturnPayload<ReturnType<T>>;
 export declare type MakeAsyncReturn<T extends AsyncFunction> = (...args: Parameters<T>) => Promise<MakeReturnPayload<ReturnType<T>>>;
 export declare type MakeTryReturn<T extends Function> = IsAsyncFunction<T> extends true ? MakeAsyncReturn<T> : MakeSyncReturn<T>;
